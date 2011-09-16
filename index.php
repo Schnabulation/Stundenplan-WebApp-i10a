1d0e92eea87230de00ad332d8de7925d56429e98
Steve Heller <steve.heller@cs2.ch>
2011-09-16 13:33:05 +0200

    Initial commit of all files

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta content="yes" name="apple-mobile-web-app-capable" />
+<link rel="apple-touch-icon" href="images/touchicon.png"/>
+<link href="images/startup.png" rel="apple-touch-startup-image" />
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<meta content="minimum-scale=1.0, width=device-width, maximum-scale=0.6667, user-scalable=no" name="viewport" />
+<link href="css/style.css" rel="stylesheet" media="screen" type="text/css" />
+<script src="javascript/functions.js" type="text/javascript"></script>
+<title>Stundenplan i10a</title>
+<meta content="keyword1,keyword2,keyword3" name="keywords" />
+<meta content="Description of your page" name="description" />
+</head>
+
+<body>
+
+<div id="topbar">
+  <div id="title">Stundenplan i10a</div>
+</div>
+
+<div id="content">
+  <div style="text-align:right; padding-right:20px;"><img height="35px" alt="logo" src="images/logo.jpg" /> </div>
+  <span class="graytitle">Stundenplan</span>
+  <ul class="pageitem">
+    <li class="textbox">
+      <p>Stundenplan der Klasse i10a, Herbstsemester 2011</p>
+    </li>
+  </ul>
+   
+  <ul class="pageitem">
+    <li class="menu">
+      <a href="tuesday.html"><span class="name">Dienstag</span><span class="arrow"> </span></a>
+    </li>
+    <li class="menu">
+      <a href="wednesday.html"><span class="name">Mittwoch</span><span class="arrow"> </span></a>
+    </li>
+    <li class="menu">
+    	<a href="thursday.html"><span class="name">Donnerstag</span><span class="arrow"> </span></a>
+    </li>   
+  </ul>
+  
+    <ul class="pageitem">
+    <li class="menu">
+      <a href="blueweeks.html"><span class="name">Blaue Wochen</span><span class="arrow"> </span></a>
+    </li>  
+  </ul>
+  
+      <ul class="pageitem">
+    <li class="menu">
+      <a href="changelog.html"><span class="name">Changelog</span><span class="arrow"> </span></a>
+    </li>  
+    <li class="menu">
+      <a href="impress.html"><span class="name">Impressum</span><span class="arrow"> </span></a>
+    </li>  
+  </ul>
+  
+
+</div>
+
+<div id="footer">
+	<!-- Support iWebKit by sending us traffic; please keep this footer on your page, consider it a thank you for our work :-) -->
+	<p class="noeffect">Version 1.1.0</p>
+  <a class="noeffect" href="http://snippetspace.com">Powered by iWebKit</a></div>
+
+</body>
+
+</html>
