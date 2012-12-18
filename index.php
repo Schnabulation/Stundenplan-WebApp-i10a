055615b0adb710f30d8a221ab811ec5917c1ec91
Schnabulation <steve.heller@schnabulation.ch>
2012-12-18 20:01:37 +0100

    Outsource header

--- index.php
+++ index.php
@@ -1,18 +1,9 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 
-<head>
-<meta content="yes" name="apple-mobile-web-app-capable" />
-<link rel="apple-touch-icon" href="images/touchicon.png"/>
-<link href="images/startup.png" rel="apple-touch-startup-image" />
-<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
-<meta content="minimum-scale=1.0, width=device-width, maximum-scale=0.6667, user-scalable=no" name="viewport" />
-<link href="css/style.css" rel="stylesheet" media="screen" type="text/css" />
-<script src="javascript/functions.js" type="text/javascript"></script>
-<title>Stundenplan i10a</title>
-<meta content="keyword1,keyword2,keyword3" name="keywords" />
-<meta content="Description of your page" name="description" />
-</head>
+<?php
+include 'header.html';
+?>
 
 <body>
 
